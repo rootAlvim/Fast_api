0c4181f22b5809ddef4f81b30399cbafb19a3af7
Álvaro  Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-08-03 15:29:53 -0300

    Minha alteração

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,9 @@
+from http import HTTPStatus
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/',status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá Mundo!'}
 
